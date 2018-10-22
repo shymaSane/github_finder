3ae4381c3f93db91aa56b6b1150e974aa6d1929d
shymaSane <shymaSane1@gmail.com>
2018-10-22 11:57:44 +0300

    successful request to github api to fetch user

---
+++ js/main.js
@@ -0,0 +1,20 @@
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    //1- getting the search input
+    let search = document.getElementById('search_user');
+    const clientId = '7c55f2d7af345f500ff9';
+    const clientSecret = '5ecf2d38f0f46fd806f28743ee07ffa6c651e7a1';
+
+    //2- add keyup event 
+    search.addEventListener('keyup', getUser)
+
+    function getUser(){
+        let user = search.value;
+        
+        fetch(`http://api.github.com/users/${user}?client_id=${clientId}&client_secret=${clientSecret}`)
+        .then((res) => res.json())
+        .then((data) => console.log(data))
+    }
+    
+  });
