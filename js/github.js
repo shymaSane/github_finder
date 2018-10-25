30b1ff31a244d2f5ab62a3cf233477bff3b8652a
shymaSane <shymaSane1@gmail.com>
2018-10-25 12:57:38 +0300

    get user from github.js successfuly

---
+++ js/github.js
@@ -0,0 +1,16 @@
+'use strict'
+
+class Github {
+    constructor(){
+        this._clientId = '7c55f2d7af345f500ff9';
+        this._clientSecret = '5ecf2d38f0f46fd806f28743ee07ffa6c651e7a1';
+    }
+
+    async getUsers(user){
+        let fetchUser = await fetch(`http://api.github.com/users/${user}?client_id=${this._clientId}&client_secret=${this._clientSecret}`);
+        let profile = await fetchUser.json();
+        return {
+            profile
+        }
+    }
+}
--- js/main.js
+++ js/main.js
@@ -3,21 +3,21 @@
 document.addEventListener("DOMContentLoaded", function() {
     //1- getting the search input
     let search = document.getElementById('search_user');
-    const clientId = '7c55f2d7af345f500ff9';
-    const clientSecret = '5ecf2d38f0f46fd806f28743ee07ffa6c651e7a1';
-
-    //2- add keyup event 
-    search.addEventListener('keyup', getUser)
-
-    function getUser(){
-        let user = search.value;
+    let github = new Github;
+    //2- add keyup event to input result
+    search.addEventListener('keyup', (e) => {
+        //current input value
+        let currentUser = e.target.value;
+        //fetch user info
+        if(currentUser !== ""){
+            github.getUsers(currentUser)
+            .then(data => {
+                console.log(data)
+            })
+        }
         
-        fetch(`http://api.github.com/users/${user}?client_id=${clientId}&client_secret=${clientSecret}`)
-        .then((res) => res.json())
-        .then((data) => {
-            console.log(data);
+        
+    });
 
-        })
-    }
     
   });
